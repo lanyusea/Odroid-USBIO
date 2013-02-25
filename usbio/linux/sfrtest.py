ced83cc390a07baea74e264a89f8744ec97f93f7
Markham Thomas <mlinuxguy@server.fake>
2013-02-25 17:45:47 -0600

    updated sfrtest to show new constant use
    created sfr_constants.py file for referencing the constants

--- usbio/linux/sfrtest.py
+++ usbio/linux/sfrtest.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
-# Markham Thomas  2/17/2013
+# Markham Thomas  2/25/2013
 # SFR test program
+#   Updated to use the constants file:  sfr_constants.py
+# and the class Bunch()
 from hk_usb_io import *
+from sfr_constants import *
 import sys
 import time
 
@@ -12,5 +15,8 @@ print module_version()		# print python module version
 print rom_version(usb)		# print rom version
 print "---------- output -----------"
 
-a = sfr_get_reg(usb, 0xcf)
+# instantiate your class of pic_registers
+ior = Bunch(pic_registers)		# uses the sfr_constants.py file
+
+a = sfr_get_reg(usb, ior.SSP1BUF)	# easy reference to the PIC SFR's
 print hex(a)
